071e673bbaaaba70180a2a0557ce6f23dbe960fe
Andrew Laignel <andrew@wearefinally.co.uk>
2015-04-27 11:32:58 +0100

    Fix

--- angular.js
+++ angular.js
@@ -346,7 +346,7 @@ function forEach(obj, iterator, context) {
 }
 
 function forEachSorted(obj, iterator, context) {
-  var keys = Object.keys(obj).sort();
+  var keys = Object.keys(obj); // .sort();
   for (var i = 0; i < keys.length; i++) {
     iterator.call(context, obj[keys[i]], keys[i]);
   }
